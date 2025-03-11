f92e93c204735d7f3ae354741d3875b54a2a7976
Ishara-Peiris <peirisbhid.21@uom.lk>
2025-03-11 23:47:51 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 import PartOne from './components/PartOne';
 import Part2 from './components/Tutorial/Part2';
+import Header from './components/Header';
+
 
 
 
@@ -13,11 +15,13 @@ import Part2 from './components/Tutorial/Part2';
 function App() {
   return (
     <div className="App">
+      <Header/>
  
  <PartOne/>
  <Part2 name="dilrukshi">ishara</Part2>
  <Part2 name="peiris"><h1>name</h1></Part2>
  <Part2 name="Bamina"><button>find me</button></Part2>
+
 
  
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function header() {
+  return (
+    <div>
+      <nav>
+        <ul>
+            <li>
+                Home
+            </li>
+            <li>
+                About us
+            </li><li>
+                Appointment
+            </li><li>
+                Careers
+            </li><li>
+                Contact
+            </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
