76a8ff4ae19a4be868f428113350d5304e3cda66
Ishara-Peiris <peirisbhid.21@uom.lk>
2025-03-12 23:47:38 +0530

    event

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 export default function event() {
+    function find(){
+        console.log("button is clicked");
+    }
   return (
     <div>
       <h1>React event</h1>
       
-      <button>1</button>
+      <button onClick={find}>click here</button>
     </div>
   )
 }
