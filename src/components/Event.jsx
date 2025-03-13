a810d32381741690f110b1c535f3f25f675e1681
Ishara-Peiris <peirisbhid.21@uom.lk>
2025-03-13 20:27:55 +0530

    number

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function event() {
+export default function Event() {
 
     function find(){
         console.log("button is clicked");
         alert("danger");
     }
-    const number=0;
+    const [count,setCount] = useState(0);
 
+    
+    
   return (
+
     <div>
       <h1>React event</h1>
-      
-      <button onClick={number}>number</button>
+      <h1>COUNT:{count}</h1>
+      <button onClick={()=>setCount(count+2)}>number</button>
       
       <button onClick={find}>click here</button>
     </div>
