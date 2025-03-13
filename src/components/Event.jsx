4c1cb226929dd32f4a53d0f2e11c5028f1a86a7c
Ishara-Peiris <peirisbhid.21@uom.lk>
2025-03-13 20:15:40 +0530

    number

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 
 export default function event() {
+
     function find(){
         console.log("button is clicked");
         alert("danger");
     }
+    const number=0;
+
   return (
     <div>
       <h1>React event</h1>
       
-      <button onClick={find()}>click here</button>
+      <button onClick={number}>number</button>
+      
+      <button onClick={find}>click here</button>
     </div>
   )
 }
